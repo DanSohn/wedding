5e0d75af2138e2b0680a2525a6b26047b239416d
Daniel Sohn <daniel.sohn@flexjourney.com>
2025-02-18 20:22:38 -0700

    build vite as app

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  plugins: [react()],
   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/main.tsx'),
-      name: 'CarinaDanielWeddingPlanning',
-      formats: ['es'],
-      fileName: 'index',
-    },
+    outDir: './dist',
   },
   resolve: {
     alias: {
