f25265e7ce498c634903b4e0d400cce656c2ff8f
Daniel Sohn <daniel.sohn@flexjourney.com>
2025-08-16 17:44:37 -0600

    move robots to public and add meta tag

--- src/pages/rsvp.tsx
+++ src/pages/rsvp.tsx
@@ -5,7 +5,7 @@ export default function RsvpPage() {
   return (
     <div className={classes.page}>
       <h1>RSVP Page</h1>
-      <p>Please RSVP by June 30 2026</p>
+      <p>Please RSVP by March 31 2026</p>
       <RsvpForm />
     </div>
   );
