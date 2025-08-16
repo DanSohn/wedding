0a20d4ab7e4fde50b3b8ca58cad9c83e0ef3b4af
Daniel Sohn <daniel.sohn@flexjourney.com>
2025-08-16 17:36:05 -0600

    fix ts error

--- src/pages/rsvp.tsx
+++ src/pages/rsvp.tsx
@@ -1,11 +1,9 @@
-import LandingImage from '../components/generic/header/header-image';
 import RsvpForm from '../components/rsvp-form';
 import classes from './rsvp.module.css';
 
 export default function RsvpPage() {
   return (
     <div className={classes.page}>
-      <LandingImage src="/wedding_stock.jpg" />
       <h1>RSVP Page</h1>
       <p>Please RSVP by June 30 2026</p>
       <RsvpForm />
