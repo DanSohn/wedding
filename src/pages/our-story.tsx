0d9aaa978c41c86d966f9fed6e30e6f97140f672
Daniel Sohn <daniel.sohn@flexjourney.com>
2025-08-20 19:38:36 -0600

    update our story

--- src/pages/our-story.tsx
+++ src/pages/our-story.tsx
@@ -5,7 +5,10 @@ function OurStoryPage() {
   return (
     <div className={classes.page}>
       <Title order={1}>Our Story</Title>
-      <Text>Carina fell in love with Daniel, he asked her out, and the rest is history</Text>
+      <Text>
+        Carina fell in love with Daniel HARD, he asked her out, they got a dog and a cat and the
+        rest is history
+      </Text>
     </div>
   );
 }
