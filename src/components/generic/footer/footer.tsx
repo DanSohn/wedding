62c1832cae83c4e8735f566f0ac108cbfb08244f
Daniel Sohn <daniel.sohn@flexjourney.com>
2026-01-05 00:34:27 -0700

    update footer

--- src/components/generic/footer/footer.tsx
+++ src/components/generic/footer/footer.tsx
@@ -4,10 +4,12 @@ import classes from './footer.module.css';
 export default function Footer() {
   return (
     <div className={classes.container}>
-      <Title order={1}>Carina & daniel</Title>
-      <Divider my="md" classNames={{ root: classes.divider }} />
-      <Title order={2}>11.23.2026</Title>
-      <Text className={classes.finalText}>I should've used WIX or something</Text>
+      <div className={classes.content}>
+        <Title order={1}>Carina & daniel</Title>
+        <Divider my="md" classNames={{ root: classes.divider }} />
+        <Title order={2}>11.23.2026</Title>
+        <Text className={classes.finalText}>I should've used WIX or something</Text>
+      </div>
     </div>
   );
 }
--- src/pages/rsvp.tsx
+++ src/pages/rsvp.tsx
@@ -4,8 +4,7 @@ import classes from './rsvp.module.css';
 export default function RsvpPage() {
   return (
     <div className={classes.page}>
-      <h1>RSVP Page</h1>
-      <p>Please RSVP by March 31 2026</p>
+      <p>Please RSVP by April 30 2026</p>
       <RsvpForm />
     </div>
   );
